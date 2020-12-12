10f09c6de8cc550aaca0d5ab34b4f67fba5e57bd
Franca <gvictor1596@gmail.com>
2020-12-12 09:58:04 -0300

    test: ensure HttpAdapter calls post with correct body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -9,7 +11,8 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({@required String url, @required String method}) async {
+  Future<void> request(
+      {@required String url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
@@ -18,6 +21,7 @@ class HttpAdapter {
     await client.post(
       url,
       headers: headers,
+      body: jsonEncode(body)
     );
   }
 }
@@ -32,17 +36,20 @@ void main() {
   setUp(() {
     client = ClientSpy();
     sut = HttpAdapter(client);
-    url = faker.internet.httpsUrl();
+    url = faker.internet.httpUrl();
   });
 
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      verify(client.post(url, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      }));
+      verify(client.post(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: '{"any_key":"any_value"}'));
     });
   });
 }
