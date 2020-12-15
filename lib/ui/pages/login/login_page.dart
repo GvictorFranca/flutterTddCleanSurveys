bc08a57c79bb88500d2c42120e71d25c65c9e21c
Gabriel V F Melo <gabrielvfmelo@MacBook-Pro-de-Gabriel.local>
2020-12-15 09:38:40 -0300

    feat: ensure LoginPage close all streams on dispose

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -3,17 +3,27 @@ import 'package:flutter/material.dart';
 import '../../components/components.dart';
 import '../pages.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   final LoginPresenter presenter;
 
   const LoginPage(this.presenter);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  void dispose() {
+    super.dispose();
+    widget.presenter.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Builder(
         builder: (context) {
-          presenter.isLoadingStream.listen((isLoading) {
+          widget.presenter.isLoadingStream.listen((isLoading) {
             if (isLoading) {
               showDialog(
                   context: context,
@@ -42,7 +52,7 @@ class LoginPage extends StatelessWidget {
             }
           });
 
-          presenter.mainErrorStream.listen((error) {
+          widget.presenter.mainErrorStream.listen((error) {
             if (error != null) {
               Scaffold.of(context).showSnackBar(SnackBar(
                 backgroundColor: Colors.red[900],
@@ -66,7 +76,7 @@ class LoginPage extends StatelessWidget {
                     child: Column(
                       children: [
                         StreamBuilder<String>(
-                            stream: presenter.emailErrorStream,
+                            stream: widget.presenter.emailErrorStream,
                             builder: (context, snapshot) {
                               return TextFormField(
                                   decoration: InputDecoration(
@@ -79,13 +89,13 @@ class LoginPage extends StatelessWidget {
                                       errorText: snapshot.data?.isEmpty == true
                                           ? null
                                           : snapshot.data),
-                                  onChanged: presenter.validateEmail,
+                                  onChanged: widget.presenter.validateEmail,
                                   keyboardType: TextInputType.emailAddress);
                             }),
                         Padding(
                           padding: const EdgeInsets.only(top: 8.0, bottom: 32),
                           child: StreamBuilder<String>(
-                              stream: presenter.passwordErrorStream,
+                              stream: widget.presenter.passwordErrorStream,
                               builder: (context, snapshot) {
                                 return TextFormField(
                                   decoration: InputDecoration(
@@ -96,17 +106,17 @@ class LoginPage extends StatelessWidget {
                                       errorText: snapshot.data?.isEmpty == true
                                           ? null
                                           : snapshot.data),
-                                  onChanged: presenter.validatePassword,
+                                  onChanged: widget.presenter.validatePassword,
                                   obscureText: true,
                                 );
                               }),
                         ),
                         StreamBuilder<bool>(
-                            stream: presenter.isFormValidStream,
+                            stream: widget.presenter.isFormValidStream,
                             builder: (context, snapshot) {
                               return RaisedButton(
                                 onPressed: snapshot.data == true
-                                    ? presenter.auth
+                                    ? widget.presenter.auth
                                     : null,
                                 child: Text('Entrar'),
                               );
