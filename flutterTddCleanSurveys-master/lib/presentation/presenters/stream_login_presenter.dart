89f33e13d704b5cc312f674ea3ed5d2c138466c6
Gabri Franca <gvictor1596@gmail.com>
2020-12-16 14:26:32 -0300

    test: ensure StreamLoginPresenter emits form valid event if any field is invalid

--- flutterTddCleanSurveys-master/lib/presentation/presenters/stream_login_presenter.dart
+++ flutterTddCleanSurveys-master/lib/presentation/presenters/stream_login_presenter.dart
@@ -4,36 +4,44 @@ import 'package:meta/meta.dart';
 import 'package:flutterClean/presentation/dependencies/validation.dart';
 
 class LoginState {
+  String email;
+  String password;
   String emailError;
   String passwordError;
-  bool get isFormValid => false;
-}
 
-var _state = LoginState();
+  bool get isFormValid =>
+      emailError == null &&
+      passwordError == null &&
+      email != null &&
+      password != null;
+}
 
 class StreamLoginPresenter {
   final Validation validation;
   final _controller = StreamController<LoginState>.broadcast();
+  var _state = LoginState();
 
   Stream<String> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError).distinct();
+      _controller?.stream?.map((state) => state.emailError)?.distinct();
   Stream<String> get passwordErrorStream =>
-      _controller.stream.map((state) => state.passwordError).distinct();
+      _controller?.stream?.map((state) => state.passwordError)?.distinct();
   Stream<bool> get isFormValidStream =>
-      _controller.stream.map((state) => state.isFormValid).distinct();
+      _controller?.stream?.map((state) => state.isFormValid)?.distinct();
 
   StreamLoginPresenter({
     @required this.validation,
   });
 
-  void _update() => _controller.add(_state);
+  void _update() => _controller?.add(_state);
 
   void validateEmail(String email) {
+    _state.email = email;
     _state.emailError = validation.validate(field: 'email', value: email);
     _update();
   }
 
   void validatePassword(String password) {
+    _state.password = password;
     _state.passwordError =
         validation.validate(field: 'password', value: password);
     _update();
