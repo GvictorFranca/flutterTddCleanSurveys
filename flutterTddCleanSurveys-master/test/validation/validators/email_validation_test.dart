586ccf075edc2a5bf82b8c4eb0ca0bb0ba7534d7
Gabri Franca <gvictor1596@gmail.com>
2020-12-17 10:33:19 -0300

    test: ensure EmailVAlidation returns null if email is empity or null

---
+++ flutterTddCleanSurveys-master/test/validation/validators/email_validation_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutterClean/validation/protocols/field_validation.dart';
+import 'package:test/test.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+void main() {
+  EmailValidation sut;
+
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empity', () {
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is empity', () {
+    expect(sut.validate(null), null);
+  });
+}
