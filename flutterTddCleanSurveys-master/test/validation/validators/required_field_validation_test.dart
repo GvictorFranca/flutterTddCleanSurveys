92ede0c5590d2ee7a2235093ac096e100c138167
Gabri Franca <gvictor1596@gmail.com>
2020-12-17 10:03:06 -0300

    test: ensure RequiredFieldValidation returns error if value is empity

--- flutterTddCleanSurveys-master/test/validation/validators/required_field_validation_test.dart
+++ flutterTddCleanSurveys-master/test/validation/validators/required_field_validation_test.dart
@@ -10,7 +10,9 @@ class RequiredFieldValidation implements FieldValidation {
 
   RequiredFieldValidation(this.field);
 
-  String validate(String value) {}
+  String validate(String value) {
+    return value.isEmpty ? 'Campo Obrigatiorio' : null;
+  }
 }
 
 void main() {
@@ -20,5 +22,13 @@ void main() {
     final error = sut.validate('any_value');
 
     expect(error, null);
+  });
+
+  test('Should return error if value is empity', () {
+    final sut = RequiredFieldValidation('any_field');
+
+    final error = sut.validate('');
+
+    expect(error, 'Campo Obrigatiorio');
   });
 }
