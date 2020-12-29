b96f0e4fac36048ee11106409858448bb5094182
Gabri Franca <gvictor1596@gmail.com>
2020-12-29 17:38:45 -0300

    ensure GetxLoginPresenter emits error if SaveCurrentAccount fails

--- flutterTddCleanSurveys-master/test/presentation/presenters/getx_login_presenter_test.dart
+++ flutterTddCleanSurveys-master/test/presentation/presenters/getx_login_presenter_test.dart
@@ -42,6 +42,13 @@ void main() {
     mockAuthenticationCall().thenThrow(error);
   }
 
+  PostExpectation mockSaveCurrentAccountCall() =>
+      when(authentication.auth(any));
+
+  void mockSaveCurrentAccountError() {
+    mockSaveCurrentAccountCall().thenThrow(DomainError.unexpected);
+  }
+
   setUp(() {
     validation = ValidationSpy();
     authentication = AuthenticationSpy();
@@ -181,6 +188,19 @@ void main() {
 
   test('Should emit correct events on UnexpectedError', () async {
     mockAuthenticationError(DomainError.unexpected);
+
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream.listen(expectAsync1((error) =>
+        expect(error, 'Algo de errado aconteceu. Tente novamente em breve')));
+
+    await sut.auth();
+  });
+
+  test('Should emit UnexpectedError if SaveCurrentAccount fails', () async {
+    mockSaveCurrentAccountError();
 
     sut.validateEmail(email);
     sut.validatePassword(password);
