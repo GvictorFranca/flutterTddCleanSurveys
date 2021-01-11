7ccb80a7eb0080c93ccb27d30d6cafe03b1aca37
Gabri Franca <gvictor1596@gmail.com>
2021-01-11 15:52:21 -0300

    ensure LocalLoadCurrentACcount throws UnexpectedError if fetchSecure fails

--- flutterTddCleanSurveys-master/test/data/UseCases/load_current_account/load_local_current_account_test.dart
+++ flutterTddCleanSurveys-master/test/data/UseCases/load_current_account/load_local_current_account_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutterClean/domain/entities/entities.dart';
+import 'package:flutterClean/domain/helpers/domain_error.dart';
 import 'package:flutterClean/domain/usecases/load_current_account.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -11,8 +12,12 @@ class LocalLoadCurrentAccount implements LoadCurrentAccount {
   LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
 
   Future<AccountEntity> load() async {
-    final token = await fetchSecureCacheStorage.fetchSecure('token');
-    return AccountEntity(token);
+    try {
+      final token = await fetchSecureCacheStorage.fetchSecure('token');
+      return AccountEntity(token);
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
   }
 }
 
@@ -28,9 +33,15 @@ void main() {
   LocalLoadCurrentAccount sut;
   String token;
 
+  PostExpectation mockFetchSecureCall() =>
+      when(fetchSecureCacheStorage.fetchSecure(any));
+
   void mockFetchSecure() {
-    when(fetchSecureCacheStorage.fetchSecure(any))
-        .thenAnswer((_) async => token);
+    mockFetchSecureCall().thenAnswer((_) async => token);
+  }
+
+  void mockFetchSecureError() {
+    mockFetchSecureCall().thenThrow(Exception());
   }
 
   setUp(() {
@@ -53,5 +64,14 @@ void main() {
     final account = await sut.load();
 
     expect(account, AccountEntity(token));
+  });
+
+  test('Should throw UnexpectedError if FetchSecureCacheStorage throws',
+      () async {
+    mockFetchSecureError();
+
+    final future = await sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
