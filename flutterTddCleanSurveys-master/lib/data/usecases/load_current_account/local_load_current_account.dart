c4473225e298d3637a5c5404ccdb219dd0e5a8be
Gabri Franca <gvictor1596@gmail.com>
2021-01-11 16:10:08 -0300

    move production code to their own files

---
+++ flutterTddCleanSurveys-master/lib/data/usecases/load_current_account/local_load_current_account.dart
@@ -0,0 +1,20 @@
+import 'package:flutterClean/data/cache/cache.dart';
+import 'package:flutterClean/domain/entities/entities.dart';
+import 'package:flutterClean/domain/helpers/helpers.dart';
+import 'package:flutterClean/domain/usecases/usecases.dart';
+import 'package:meta/meta.dart';
+
+class LocalLoadCurrentAccount implements LoadCurrentAccount {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
+
+  Future<AccountEntity> load() async {
+    try {
+      final token = await fetchSecureCacheStorage.fetchSecure('token');
+      return AccountEntity(token);
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
+  }
+}
