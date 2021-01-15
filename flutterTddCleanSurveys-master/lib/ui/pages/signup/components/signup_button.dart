ab42337f348efaf9413d637ef066e15d4512e625
Gabri Franca <gvictor1596@gmail.com>
2021-01-15 18:12:30 -0300

    ensure SignUpPage load with correct state

---
+++ flutterTddCleanSurveys-master/lib/ui/pages/signup/components/signup_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignUpButon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: null,
+      child: Text('Entrar'.toUpperCase()),
+    );
+  }
+}
