e82aa7e68636ecbbb54bacce6f6e7d2492720784
Gabri Franca <gvictor1596@gmail.com>
2021-01-18 10:52:20 -0300

    ensure GetX SignUpPresenter emits correct error on password changed

--- flutterTddCleanSurveys-master/test/presentation/presenters/getx_signup_presenter_test.dart
+++ flutterTddCleanSurveys-master/test/presentation/presenters/getx_signup_presenter_test.dart
@@ -14,6 +14,7 @@ void main() {
   ValidationSpy validation;
   String email;
   String name;
+  String password;
 
   PostExpectation mockValidationCall(String field) => when(validation.validate(
       field: field == null ? anyNamed('field') : field,
@@ -31,6 +32,7 @@ void main() {
     );
     email = faker.internet.email();
     name = faker.person.name();
+    password = faker.internet.password();
     mockValidation();
   });
 
@@ -113,6 +115,48 @@ void main() {
 
       sut.validateName(name);
       sut.validateName(name);
+    });
+  });
+
+  group('Password Group', () {
+    test('Should call Validation with correct Password', () {
+      sut.validatePassword(password);
+
+      verify(validation.validate(field: 'password', value: password)).called(1);
+    });
+
+    test('Should emit invalidFieldError if password is invalid', () {
+      mockValidation(value: ValidationError.invalidField);
+
+      sut.passwordErrorStream
+          .listen(expectAsync1((error) => expect(error, UIError.invalidField)));
+      sut.isFormValidStream
+          .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+      sut.validatePassword(password);
+      sut.validatePassword(password);
+    });
+
+    test('Should emit requiredFieldError if password is empty', () {
+      mockValidation(value: ValidationError.requiredField);
+
+      sut.passwordErrorStream.listen(
+          expectAsync1((error) => expect(error, UIError.requiredField)));
+      sut.isFormValidStream
+          .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+      sut.validatePassword(password);
+      sut.validatePassword(password);
+    });
+
+    test('Should emit null if validation succeeds', () {
+      sut.passwordErrorStream
+          .listen(expectAsync1((error) => expect(error, null)));
+      sut.isFormValidStream
+          .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+      sut.validatePassword(password);
+      sut.validatePassword(password);
     });
   });
 }
