b687fa2601de27a7623aea0159dff614c3accc6b
Gabri Franca <gvictor1596@gmail.com>
2021-01-18 15:52:02 -0300

    ensure GetXSignupPresenter emits error if addAccountFails

--- flutterTddCleanSurveys-master/test/presentation/presenters/getx_signup_presenter_test.dart
+++ flutterTddCleanSurveys-master/test/presentation/presenters/getx_signup_presenter_test.dart
@@ -43,6 +43,10 @@ void main() {
     mockAddAccountCall().thenAnswer((_) async => AccountEntity(token: token));
   }
 
+  void mockAddAccountError(DomainError error) {
+    mockAddAccountCall().thenThrow(error);
+  }
+
   PostExpectation mockSaveCurrentAccountCall() =>
       when(saveCurrentAccount.save(any));
 
@@ -298,6 +302,32 @@ void main() {
     sut.validatePasswordConfirmation(passwordConfirmation);
 
     expectLater(sut.isLoadingStream, emits(true));
+
+    await sut.signup();
+  });
+  test('Should emit correct events on EmailInUse Error', () async {
+    mockAddAccountError(DomainError.emailInUse);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.emailInUse)));
+
+    await sut.signup();
+  });
+
+  test('Should emit correct events on UnexpectedError', () async {
+    mockAddAccountError(DomainError.unexpected);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.unexpected)));
 
     await sut.signup();
   });
