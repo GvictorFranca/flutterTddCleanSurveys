9923a95920eda4687c57b89b8548bd72816e6a8d
Gabri Franca <gvictor1596@gmail.com>
2021-01-22 12:28:23 -0300

    move sut creation to a setUp helper method

---
+++ flutterTddCleanSurveys-master/test/data/UseCases/load_surveys/remote_load_surveys_test.dart
@@ -0,0 +1,35 @@
+import 'package:faker/faker.dart';
+import 'package:flutterClean/data/http/http.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class RemoteLoadSurveys {
+  final String url;
+  final HttpClient httpClient;
+
+  RemoteLoadSurveys({@required this.url, @required this.httpClient});
+
+  Future<void> load() async {
+    await httpClient.request(url: url, method: 'get');
+  }
+}
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+void main() {
+  RemoteLoadSurveys sut;
+  HttpClientSpy httpClient;
+  String url;
+
+  setUp(() {
+    url = faker.internet.httpUrl();
+    httpClient = HttpClientSpy();
+    sut = RemoteLoadSurveys(url: url, httpClient: httpClient);
+  });
+  test('Should call HttpClient with correct values', () async {
+    await sut.load();
+
+    verify(httpClient.request(url: url, method: 'get'));
+  });
+}
