6f81366ed7a47288c5bb72d67862230c49af28a1
Gabri Franca <gvictor1596@gmail.com>
2021-01-25 16:30:43 -0300

    move loadDAta after stream listen

--- flutterTddCleanSurveys-master/lib/ui/pages/surveys/surveys_page.dart
+++ flutterTddCleanSurveys-master/lib/ui/pages/surveys/surveys_page.dart
@@ -14,7 +14,6 @@ class SurveysPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    presenter.loadData();
     return Scaffold(
       appBar: AppBar(
         title: Text(R.string.surveys),
@@ -30,6 +29,7 @@ class SurveysPage extends StatelessWidget {
             }
           });
 
+          presenter.loadData();
           return StreamBuilder<List<SurveyViewModel>>(
             stream: presenter.surveysStream,
             builder: (context, snapshot) {
