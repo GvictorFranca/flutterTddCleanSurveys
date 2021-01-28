85714674d94cc29e26938dd7104ae195923bb676
Gabri Franca <gvictor1596@gmail.com>
2021-01-28 11:50:49 -0300

    ensure composite returns remote data

--- flutterTddCleanSurveys-master/test/main/composite/load_surveys_with_local_fallback_test.dart
+++ flutterTddCleanSurveys-master/test/main/composite/load_surveys_with_local_fallback_test.dart
@@ -1,11 +1,12 @@
 import 'package:faker/faker.dart';
 import 'package:flutterClean/data/usecases/load_surveys/load_surveys.dart';
 import 'package:flutterClean/domain/entities/entities.dart';
+import 'package:flutterClean/domain/usecases/usecases.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class RemoteLoadSurveysWithLocalFallback {
+class RemoteLoadSurveysWithLocalFallback implements LoadSurveys {
   final RemoteLoadSurveys remote;
   final LocalLoadSurveys local;
 
@@ -14,9 +15,10 @@ class RemoteLoadSurveysWithLocalFallback {
     @required this.local,
   });
 
-  Future<void> load() async {
+  Future<List<SurveyEntity>> load() async {
     final surveys = await remote.load();
     await local.save(surveys);
+    return surveys;
   }
 }
 
@@ -28,7 +30,7 @@ void main() {
   RemoteLoadSurveysSpy remote;
   RemoteLoadSurveysWithLocalFallback sut;
   LocalLoadSurveysSpy local;
-  List<SurveyEntity> surveys;
+  List<SurveyEntity> remoteSurveys;
 
   List<SurveyEntity> mockSurveys() => [
         SurveyEntity(
@@ -39,8 +41,8 @@ void main() {
       ];
 
   void mockRemoteload() {
-    surveys = mockSurveys();
-    when(remote.load()).thenAnswer((_) async => surveys);
+    remoteSurveys = mockSurveys();
+    when(remote.load()).thenAnswer((_) async => remoteSurveys);
   }
 
   setUp(() {
@@ -58,6 +60,12 @@ void main() {
   test('Should call  local save with remote data', () async {
     await sut.load();
 
-    verify(local.save(surveys)).called(1);
+    verify(local.save(remoteSurveys)).called(1);
+  });
+
+  test('Should return  remote data', () async {
+    final surveys = await sut.load();
+
+    expect(surveys, remoteSurveys);
   });
 }
